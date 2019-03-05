e102842f885655e00a76d61543a1d22c9c29cf2b
tojawawel <bgawel93@gmail.com>
2019-03-05 16:07:50 +0100

    add rspec to travis config

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'welcome page' do
   scenario 'welcome message' do
     visit('/')
-    expect(page).to have_content('Welcome!!!')
+    expect(page).to have_content('Welcome here!!!')
   end
 end
